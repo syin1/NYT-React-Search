6ccad66d08bc64988b276bc91ec8e798c9e6daaa
syin <shaomingyinut@gmail.com>
2018-11-24 18:24:43 -0500

    add mongodb credentials

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /////////////////////////////////////////////// /* Imports */ //////////////////////////////////////////////////////////
 const express = require('express'); // Server
-const bodyParser = require ('body-parser'); // JSON Middleware
+const bodyParser = require('body-parser'); // JSON Middleware
 const logger = require('morgan'); // REST Logger
 const mongoose = require('mongoose'); // MongoDB ORM
-const routes = require("./routes");
-let db = require("./models"); // Require all models
+const routes = require('./routes');
+let db = require('./models'); // Require all models
 
 /////////////////////////////////////////////// /* Variables */ //////////////////////////////////////////////////////////
 let PORT = process.env.PORT || 8080;
@@ -14,30 +14,38 @@ let mongooseConnection = mongoose.connection;
 let app = express();
 
 /////////////////////////////////////////////// /* Express Middleware */ //////////////////////////////////////////////////////////
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // Allows For JSON Interactions Between Client & Server
-app.use(express.static("client/build")); // Serve Static React Pages
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Allows For JSON Interactions Between Client & Server
+app.use(express.static('client/build')); // Serve Static React Pages
 app.use(bodyParser.text());
-app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 /////////////////////////////////////////////// /* Mongoose Configurations*/ //////////////////////////////////////////////////////////
 mongoose.Promise = global.Promise; // Set up promises with mongoose
 
-mongoose.connect( // Connect to the Mongo DB
-  process.env.MONGODB_URI || "mongodb://heroku_8zqx26cl:le1fh1tcj3m2gsggd819o9ns7e@ds249398.mlab.com:49398/heroku_8zqx26cl"
+mongoose.connect(
+  // Connect to the Mongo DB
+  process.env.MONGODB_URI ||
+    'mongodb://heroku_svtzgtmn:b0i14s41prfbttfccj29m8o1sj@ds151951.mlab.com:51951/heroku_svtzgtmn'
 );
 
-mongooseConnection.on('error', console.error.bind(console, 'connection error:'));
+mongooseConnection.on(
+  'error',
+  console.error.bind(console, 'connection error:')
+);
 
 mongooseConnection.once('open', function() {
   console.log(`Sucessfully Connected to Mongo DB !`); // If Connection is successful, Console.log(Message)
 });
 
-var cors = require("cors");
+var cors = require('cors');
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 app.use(cors());
@@ -49,9 +57,7 @@ app.use(cors());
 //    );
 // });
 
-
 app.use(routes); // Add routes, both API and View
-
 
 /////////////////////////////////////////////// /* Start Server */ //////////////////////////////////////////////////////////
 app.listen(PORT, function() {
